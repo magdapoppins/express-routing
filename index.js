0644e996da7a0cffea71eb6e4036b34eae00ff90
mast900 <magdalena.stenius@securitas.se>
2018-01-10 13:45:59 +0100

    Post-route

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+const bodyParser = require('body-parser')
 const app = express();
+
+// For the app to receive POST requests, it needs a the body-parser-package
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 // Run node index.js in the console. 
 app.listen(3000, () => {
@@ -24,10 +29,24 @@ app.get('/message', (req, res) => {
 
 // Start server (replace link mlab, mongodb://<dbuser>:...)
 MongoClient.connect('CONNSTRING<3', (err, db) => {
-    var database = db.db("crudexpress");
+    var dbase = db.db("crudexpress");
     if (err) return console.log(err)
     app.listen(3000, () => {
         console.log('working on :3000')
     })
 });
 
+// The POST route
+app.post('/colors/add', (req, res, next) => {
+    var color = {
+        color_name: req.body.color_name,
+        sample_thing: req.body.sample_thing
+    };
+    dbase.collection("color").save(color, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+
+        res.send('POST successful');
+    });
+});
