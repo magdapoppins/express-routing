f2609bff83b8b89b27d4da376b91fbfc4b051f44
mast900 <magdalena.stenius@securitas.se>
2018-01-10 13:00:33 +0100

    nodemon

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const app = express();
 
-express().listen(3000, function() {
-    console.log('I am listening at :3000')
+// Run node index.js in the console. 
+app.listen(3000, function() {
+    console.log('I am listening at :3000.')
+});
+
+// GET '/'
+app.get('/', function(req, res) {
+    res.send("Ta-da, I'm here!")
+});
+
+// GET '/message'
+app.get('/message', function(req, res) {
+    res.send("Remember to make lunch for tomorrow!")
 });
