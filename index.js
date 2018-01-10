c452eb55bd63ade624a6c655638324bda1c5289c
mast900 <magdalena.stenius@securitas.se>
2018-01-10 13:15:22 +0100

    MongoDB conn

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const app = express();
 
 // Run node index.js in the console. 
-app.listen(3000, function() {
+app.listen(3000, () => {
     console.log('I am listening at :3000.')
 });
 
+// BASIC ROUTES
+
 // GET '/'
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.send("Ta-da, I'm here!")
 });
 
 // GET '/message'
-app.get('/message', function(req, res) {
+app.get('/message', (req, res) => {
     res.send("Remember to make lunch for tomorrow!")
 });
+
+// LET'S DO CRUD WITH MONGODB
+
+// Start server (replace link mlab, mongodb://<dbuser>:...)
+MongoClient.connect('YOUR-LINK', (err, db) => {
+    var database = db.db("CRUD");
+    if (err) return console.log(err);
+    app.listen(3000, () => {
+        console.log('working on :3000')
+    })
+});
+
