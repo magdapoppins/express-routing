e7166d913b41ed77839f939181c432ef06e936a3
magdapoppins <mmbste@utu.fi>
2018-01-12 20:36:36 +0100

    Runs, but POST not working.

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 // Run node index.js in the console. 
-app.listen(3000, () => {
-    console.log('I am listening at :3000.')
-});
+// app.listen(3500, () => {
+//     console.log('I am listening at :3000.')
+// });
 
 // BASIC ROUTES
 
 // GET '/'
 app.get('/', (req, res) => {
-    res.send("Ta-da, I'm here!")
+    res.send("Ta-da, I'm there!")
 });
 
 // GET '/message'
@@ -27,17 +27,30 @@ app.get('/message', (req, res) => {
 
 // LET'S DO CRUD WITH MONGODB
 
+var http=require('http');
+
+// var server=http.createServer(function(req,res){
+//     res.end('test');
+// });
+
+// server.on('listening',function(){
+//     console.log('ok, server is running');
+// });
+
+// server.listen(3000);
+
 // Start server (replace link mlab, mongodb://<dbuser>:...)
-MongoClient.connect('CONNSTRING<3', (err, db) => {
+MongoClient.connect('mongodb://spam:spam@ds247327.mlab.com:47327/crudexpress', (err, db) => {
     var dbase = db.db("crudexpress");
     if (err) return console.log(err)
-    app.listen(3000, () => {
-        console.log('working on :3000')
+    app.listen(3500, () => {
+        console.log('working on :3500')
     })
-});
+}); 
+
 
 // The POST route
-app.post('/colors/add', (req, res, next) => {
+app.post('/color/add', (req, res, next) => {
     var color = {
         color_name: req.body.color_name,
         sample_thing: req.body.sample_thing
@@ -50,3 +63,4 @@ app.post('/colors/add', (req, res, next) => {
         res.send('POST successful');
     });
 });
+
